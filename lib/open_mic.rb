88617b375d5e9eeb2b8a3292c5708f69d9f8bc91
Erin Quinn <equinn125@gmail.com>
2021-07-14 15:34:25 -0600

    add welcome users test and impl

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,6 +1,16 @@
 class OpenMic
+  attr_reader :location, :date, :performers
   def initialize(attributes)
     @location = attributes.fetch(:location)
     @date = attributes.fetch(:date)
+    @performers = []
+  end
+
+  def welcome(user)
+    @performers << user
+  end
+
+  def repeated_jokes?
+    
   end
 end
--- spec/open_mic_spec.rb
+++ spec/open_mic_spec.rb
@@ -7,4 +7,42 @@ RSpec.describe OpenMic do
     open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
     expect(open_mic).to be_instance_of(OpenMic)
   end
+
+  it 'has a location' do
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    expect(open_mic.location).to eq("Comedy Works")
+  end
+
+  it 'has a date' do
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    expect(open_mic.date).to eq("11-20-18")
+  end
+
+  it 'can have performers' do
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    expect(open_mic.performers).to eq([])
+  end
+  it 'welcomes performers' do
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    user_1 = User.new("Sal")
+    user_2 = User.new("Ali")
+    open_mic.welcome(user_1)
+    open_mic.welcome(user_2)
+
+    expect(open_mic.performers).to eq([user_1, user_2])
+  end
+  it 'can check if a joke was repeated' do
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    user_1 = User.new("Sal")
+    user_2 = User.new("Ali")
+    user_2 = User.new("Ali")
+    joke_1 = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(13, "How do you keep a lion from charging?", "Take away its credit cards.")
+    open_mic.welcome(user_1)
+    open_mic.welcome(user_2)
+    user_1.learn(joke_1)
+    user_1.learn(joke_2)
+
+    expect(open_mic.repeated_jokes?).to eq(false)
+  end
 end
