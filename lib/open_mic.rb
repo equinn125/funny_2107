8132577f95feadb63736322a07fc5c43893065f6
Erin Quinn <equinn125@gmail.com>
2021-07-14 15:23:06 -0600

    Create initialize method for open mic

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,22 +1,6 @@
 class OpenMic
-  attr_reader :location,
-              :date,
-              :performers
-
   def initialize(attributes)
-    @location = attributes[:location]
-    @date = attributes[:date]
-    @performers = []
-  end
-
-  def welcome(user)
-    @performers << user
-  end
-
-  def repeated_jokes?
-    jokes = @performers.map do |user|
-      user.jokes
-    end.flatten
-    jokes.length != jokes.uniq.length
+    @location = attributes.fetch(:location)
+    @date = attributes.fetch(:date)
   end
 end
